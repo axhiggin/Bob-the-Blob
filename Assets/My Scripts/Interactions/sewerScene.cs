46817ccc04fabc08645541e4146df927880ffbc5
axhiggin <axhiggin@ucsc.edu>
2023-12-04 17:08:47 -0800

    changed outdoor scene and added interactor

---
+++ Assets/My Scripts/Interactions/sewerScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sewerScene : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        SceneManager.LoadScene("OutdoorScene", LoadSceneMode.Single);
+        Debug.Log("press e to load next scene");
+    }
+}
